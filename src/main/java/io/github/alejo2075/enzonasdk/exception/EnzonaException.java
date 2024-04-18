91a2fad28184554619088fc0b248c9018f3965dc
Alejo2075 <alejsant75@gmail.com>
2024-04-18 12:00:41 -0500

    improved doc

--- src/main/java/io/github/alejo2075/enzonasdk/exception/EnzonaException.java
+++ src/main/java/io/github/alejo2075/enzonasdk/exception/EnzonaException.java
@@ -1,14 +1,17 @@
 package io.github.alejo2075.enzonasdk.exception;
 
 /**
- * Custom exception class for handling errors related to Enzona API interactions.
+ * Represents exceptions that are specific to EnZona API interactions.
+ * This class extends {@link Exception} and provides constructors that support both messages and causes.
+ *
+ * <p>Use this exception to indicate failures related to EnZona API operations, such as network issues, data format errors, or API limitations.</p>
  */
 public class EnzonaException extends Exception {
 
     /**
      * Constructs a new EnzonaException with the specified detail message.
      *
-     * @param message the detail message.
+     * @param message the detail message, saved for later retrieval by the getMessage() method
      */
     public EnzonaException(String message) {
         super(message);
@@ -17,8 +20,8 @@ public class EnzonaException extends Exception {
     /**
      * Constructs a new EnzonaException with the specified detail message and cause.
      *
-     * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null} value is permitted, and indicates that the cause is nonexistent or unknown.)
+     * @param message the detail message, saved for later retrieval by the getMessage() method
+     * @param cause   the cause, saved for later retrieval by the getCause() method, which may be null indicating the cause is nonexistent or unknown
      */
     public EnzonaException(String message, Throwable cause) {
         super(message, cause);
