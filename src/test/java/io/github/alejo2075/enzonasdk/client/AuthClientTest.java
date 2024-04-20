4beee63b1f322b4f00ffe336811c3f2497c80ace
Alejo2075 <alejsant75@gmail.com>
2024-04-20 15:08:22 -0500

    improved tests for AuthClientTest

--- src/test/java/io/github/alejo2075/enzonasdk/client/AuthClientTest.java
+++ src/test/java/io/github/alejo2075/enzonasdk/client/AuthClientTest.java
@@ -1,8 +1,28 @@
 package io.github.alejo2075.enzonasdk.client;
 
+import io.github.alejo2075.enzonasdk.exception.EnzonaException;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthClientTest {
 
-    /*@Mock
+    @Mock
     private HttpClient mockHttpClient;
     @Mock
     private HttpResponse mockHttpResponse;
@@ -11,14 +31,14 @@ public class AuthClientTest {
 
     private AuthClient authClient;
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
         authClient = new AuthClient("consumerKey", "consumerSecret");
         authClient.setHttpClient(mockHttpClient);
     }
 
     @Test
+    @DisplayName("getAccessToken returns valid token when response is successful")
     public void getAccessToken_Success() throws Exception {
         // Setup mock response
         when(mockHttpClient.execute(any(HttpPost.class))).thenReturn(mockHttpResponse);
@@ -30,12 +50,15 @@ public class AuthClientTest {
         String accessToken = authClient.getAccessToken();
 
         // Verify
-        assertNotNull("Access token should not be null", accessToken);
-        assertEquals("Access token should match expected value", "validToken123", accessToken);
+        assertAll("Valid access token retrieval",
+                () -> assertNotNull(accessToken, "Access token should not be null"),
+                () -> assertEquals("validToken123", accessToken, "Access token should match expected value")
+        );
     }
 
-    @Test(expected = EnzonaException.class)
-    public void getAccessToken_Failure() throws Exception {
+    @Test
+    @DisplayName("getAccessToken throws EnzonaException when response is unauthorized")
+    public void getAccessToken_Failure() throws IOException {
         // Setup mock response
         when(mockHttpClient.execute(any(HttpPost.class))).thenReturn(mockHttpResponse);
         when(mockHttpResponse.getStatusLine()).thenReturn(mockStatusLine);
@@ -43,6 +66,9 @@ public class AuthClientTest {
         when(mockHttpResponse.getEntity()).thenReturn(new StringEntity("{\"error\":\"Unauthorized\"}"));
 
         // This should throw an exception
-        authClient.getAccessToken();
-    }*/
+        EnzonaException thrown = assertThrows(EnzonaException.class, () -> authClient.getAccessToken(),
+                "Should throw an EnzonaException for unauthorized access");
+
+        assertTrue(thrown.getMessage().contains("Unauthorized"), "Exception message should include 'Unauthorized'");
+    }
 }
